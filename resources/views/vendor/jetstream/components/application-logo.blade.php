82070f9041b715d08695328cca916d869e4b029d
Miguel Angel <miguel.generic@gmail.com>
2020-10-23 17:53:37 -0500

    .

--- resources/views/vendor/jetstream/components/application-logo.blade.php
+++ resources/views/vendor/jetstream/components/application-logo.blade.php
@@ -1,1 +1,3 @@
-<img class="w-16 h-16" src="{{ asset('img/acorla-logo.png') }}" alt="Logo">
+<a href="https://www.acorla.com">
+    <img class="h-16" src="{{ asset('img/acorla-logo.png') }}" alt="Logo">
+</a>
--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,3 +1,3 @@
-<a href="/">
-    <img class="w-16 h-16" src="{{ asset('img/acorla-logo.png') }}" alt="Logo">
+<a href="https://www.acorla.com">
+    <img class="h-16" src="{{ asset('img/acorla-logo.png') }}" alt="Logo">
 </a>
