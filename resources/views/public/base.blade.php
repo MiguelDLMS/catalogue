acd54bbea47b3650c9b95bdcd5fcb562ab0a0f84
Miguel Angel <miguel.generic@gmail.com>
2020-10-24 20:55:28 -0500

    .

--- resources/views/public/base.blade.php
+++ resources/views/public/base.blade.php
@@ -20,6 +20,9 @@
                         <img src="{{ asset('img/acorla-logo-white.png') }}" alt="Logo">
                     </a>
                 </div>
+                <button class="navbar-toggler" data-toggle="collapse" type="button" id="sidebarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
                 <div class="mx-auto order-0">
                     <a class="navbar-brand mx-auto" href="{{ url('/') }}">
                         <h2>Catálogo Latam</h2>
@@ -37,13 +40,6 @@
                                     <input type="text" class="form-control" name="search" id="search" placeholder="Buscar" required>
                                 </div>
                             </form>
-                        </li>
-                        <li>
-                            <button type="button" id="sidebarCollapse" class="btn btn-dark">
-                                <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-list" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-                                    </svg>
-                            </button>
                         </li>
                     </ul>
                 </div>
