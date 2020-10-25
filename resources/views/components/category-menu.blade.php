30b5ba1210fa77cafdbc60ddc5e05cecf1b5b08d
Miguel Angel <miguel.generic@gmail.com>
2020-10-24 21:06:08 -0500

    .

--- resources/views/components/category-menu.blade.php
+++ resources/views/components/category-menu.blade.php
@@ -1,10 +1,10 @@
-<div class="col-12 float-md-left" style="height:100%">
+<div class="col-12 float-left" style="height:100%">
     <nav id="sidebar">
         <div class="p-3 sidebar-header">
             <h3 class="px1">Paises</h3>
         </div>
 
-        <ul>
+        <ul class="list-unstyled components">
             @foreach ( $categories as $category )
                 <li>
                     <a class="px-4" href="{{ url('category/'.$category->Name) }}">{{ $category->Name }}</a>
