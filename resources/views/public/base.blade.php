a8a14156b31fd35cc9d3ea4278d321e39d474025
Miguel Angel <miguel.generic@gmail.com>
2020-10-25 19:37:48 -0600

    .

--- resources/views/public/base.blade.php
+++ resources/views/public/base.blade.php
@@ -72,11 +72,25 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
         <script type="text/javascript">
             $(document).ready(function () {
-
-                $('#sidebarCollapse').on('click', function () {
-                    $('#sidebar').toggleClass('active');
+                $("#sidebar").mCustomScrollbar({
+                    theme: "minimal"
                 });
 
+                $('#dismiss, .overlay').on('click', function () {
+                    // hide sidebar
+                    $('#sidebar').removeClass('active');
+                    // hide overlay
+                    $('.overlay').removeClass('active');
+                });
+
+                $('#sidebarCollapse').on('click', function () {
+                    // open sidebar
+                    $('#sidebar').addClass('active');
+                    // fade in the overlay
+                    $('.overlay').addClass('active');
+                    $('.collapse.in').toggleClass('in');
+                    $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+                });
             });
         </script>
         @stack('javascripts')
