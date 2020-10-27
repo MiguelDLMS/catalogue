065c63624a953e23b9bdba54fdebcd470eb9bc3a
Miguel Angel <miguel.generic@gmail.com>
2020-10-26 19:14:01 -0600

    .

--- routes/web.php
+++ routes/web.php
@@ -178,6 +178,6 @@ Route::post('/request-quote', [
 ]);
 
 Route::post('/update-product-images', [
-    'uses' => 'App\Http\Controllers\MyController@update',
+    'uses' => 'App\Http\Controllers\ProductsController@update',
     'as' => 'update.product.images'
 ]);
