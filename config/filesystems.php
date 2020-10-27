6f1681b36d7cc0bfab6f44b4d08330cfcfedf1d8
Miguel Angel <miguel.generic@gmail.com>
2020-10-27 15:38:14 -0600

    .

--- config/filesystems.php
+++ config/filesystems.php
@@ -80,6 +80,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
+        public_path('storage/img/products') => storage_path('app/public/img/products'),
     ],
 
 ];
