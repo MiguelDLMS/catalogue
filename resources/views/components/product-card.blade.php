974b1377afe2506492af21a0ef677b92d1d265e4
Miguel Angel <miguel.generic@gmail.com>
2020-10-27 15:33:35 -0600

    .

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -1,6 +1,6 @@
 <div class="col-lg-4 col-md-6 mb-4">
     <div class="card h-100">
-<a href="{{ url('/product/'.$productID) }}"><img class="card-img-top" src="{{ storage_path('/img/products/'.$imageName) }}" style="height: 25vh;object-fit: cover;" alt=""></a>
+<a href="{{ url('/product/'.$productID) }}"><img class="card-img-top" src="{{ asset('storage/img/products/'.$imageName) }}" style="height: 25vh;object-fit: cover;" alt=""></a>
         <div class="card-body">
             <h4 class="card-title">
                 <a href="{{ url('/product/'.$productID) }}">{{ $productName }}</a>
