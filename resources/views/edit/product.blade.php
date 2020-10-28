74c006c9778e9b1665bc0203374aabdae3b214c6
Miguel Angel <miguel.generic@gmail.com>
2020-10-28 15:28:53 -0600

    .

--- resources/views/edit/product.blade.php
+++ resources/views/edit/product.blade.php
@@ -106,7 +106,7 @@
             
             <div class="card-footer">
                 <div class="custom-control custom-switch float-right">
-                    <input id="visible" name="visible" type="checkbox" class="custom-control-input" @if($product['visible']) checked @endif>
+                    <input id="visible" name="visible" type="checkbox" class="custom-control-input" @if($product['Visible']) checked @endif>
                     <label class="custom-control-label" for="visible">Mostrar este producto al público</label>
                 </div>
             </div>
