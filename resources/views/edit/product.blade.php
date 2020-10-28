b4506c66ae38fb3b65499161318c285d4277e914
Miguel Angel <miguel.generic@gmail.com>
2020-10-28 17:26:59 -0600

    .

--- resources/views/edit/product.blade.php
+++ resources/views/edit/product.blade.php
@@ -202,9 +202,15 @@
         <!-- /.card -->
 
         <button id="save-changes" type="submit" class="float btn btn-primary" data-toggle="tooltip" data-placement="left" title="Guardar">
-            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cloud-upload-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M8 0a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 4.095 0 5.555 0 7.318 0 9.366 1.708 11 3.781 11H7.5V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11h4.188C14.502 11 16 9.57 16 7.773c0-1.636-1.242-2.969-2.834-3.194C12.923 1.999 10.69 0 8 0zm-.5 14.5V11h1v3.5a.5.5 0 0 1-1 0z"/>
-            </svg>
+            <div id="saving-spinner" style="display: none;">
+                <span class="spinner-border spinner-border-sm" style="width: 1em; height: 1em;" role="status" aria-hidden="true"></span>
+                <span class="sr-only">Saving...</span>
+            </div>
+            <div id="save-icon">
+                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cloud-upload-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M8 0a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 4.095 0 5.555 0 7.318 0 9.366 1.708 11 3.781 11H7.5V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11h4.188C14.502 11 16 9.57 16 7.773c0-1.636-1.242-2.969-2.834-3.194C12.923 1.999 10.69 0 8 0zm-.5 14.5V11h1v3.5a.5.5 0 0 1-1 0z"/>
+                </svg>
+            </div>
         </button>
     </form>
 @endpush
@@ -305,6 +311,8 @@
 
             $("#product-form").submit(function(e) {
                 e.preventDefault();
+                $('#save-icon').hide();
+                $('#saving-spinner').show();
 
                 var formData = new FormData();
 
@@ -323,12 +331,16 @@
                     data: formData,
                     success: function(result) {
                         console.log("Success: " + result);
-                                
+                        
+                        $('#save-icon').show();
+                        $('#saving-spinner').hide();
                         $('#success-modal').modal('show');
                     },
                     error: function(data) {
                         console.log("Error: " + data);
-
+                        
+                        $('#save-icon').show();
+                        $('#saving-spinner').hide();
                         $('#error-modal').modal('hide');
                     }
                 });
