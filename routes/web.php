bde9971860c1ba0c77f4565d25843a128915a55b
Miguel Angel <miguel.generic@gmail.com>
2020-10-31 14:35:34 -0600

    .

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-use \App\Product as ProductModel;
+use App\Models\Product as ProductModel;
 
 /*
 |--------------------------------------------------------------------------
