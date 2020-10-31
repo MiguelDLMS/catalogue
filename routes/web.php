ee2557c7baaaed3a5c1d218ec8fb5a8e8bbfd108
Miguel Angel <miguel.generic@gmail.com>
2020-10-31 14:18:08 -0600

    .

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/{page?}', function ($page = 1) {
     $products = DB::table('PRODUCTS')
-        ->where('Visible', 1)
         ->paginate(15, ['ID_Product', 'Name', 'Description'], 'page', $page)
+        ->where('Visible', 1)
         ->get();
 
     $products = json_decode($products, true);
